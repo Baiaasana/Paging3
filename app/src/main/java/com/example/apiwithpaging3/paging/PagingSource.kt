9aaeabaa055ed6cf457c33bdd7fc31dfff5a3e15
Baia Asanidze <baia.asanidze.1@btu.edu.ge>
2022-08-24 13:01:47 +0400

    edit paging

--- app/src/main/java/com/example/apiwithpaging3/paging/PagingSource.kt
+++ app/src/main/java/com/example/apiwithpaging3/paging/PagingSource.kt
@@ -10,12 +10,12 @@ class PagingSource(private val api: PersonApi) : PagingSource<Int, Data>() {
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Data> {
         return try {
-            val nextPageNumber = params.key ?: 0
+            val nextPageNumber = params.key ?: 1
             val response: PersonModel = api.getPersonData(nextPageNumber)
 
             LoadResult.Page(
                 data = response.data,
-                prevKey = if (nextPageNumber > 0) nextPageNumber - 1 else null,
+                prevKey = if (nextPageNumber > 1) nextPageNumber - 1 else null,
                 nextKey = if (nextPageNumber < response.totalPages) nextPageNumber + 1 else null
             )
         } catch (e: Exception) {
